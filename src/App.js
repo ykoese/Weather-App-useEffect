03d9026901787147f39658a82c78a856c9cbd99f
mrtcologne <43711435+mrtcologne@users.noreply.github.com>
2019-10-28 22:44:49 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+import React from 'react';
+
+import './App.css';
+
+
+import Weather from "./Weather/Weather";
+import Form from "./Weather/Form";
+
+
+
+const Api_Key = "";
+
+class App extends React.Component {
+
+  state = {
+
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined
+  }
+
+  //getWeather is a method we'll use to make the api call
+  getWeather = async (e) => {
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    e.preventDefault();
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${Api_Key}`);
+    const response = await api_call.json();
+    console.log(response);
+    if (city && country) {
+      this.setState({
+        temperature: response.main.temp,
+        city: response.name,
+        country: response.sys.country,
+        humidity: response.main.humidity,
+        description: response.weather[0].description,
+        error: ""
+      })
+    } else {
+      this.setState({
+        error: "Please input search values..."
+      })
+    }
+  }
+
+  render() {
+
+    return (
+
+      <div>
+
+        <Form loadWeather={this.getWeather} />
+        <Weather
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
+
+      </div>
+
+    )
+  }
+}
+export default App;
